a012dcf57d5e5fadd501e99d74ff444d3927808a
Botao <wxt2005@gmail.com>
2024-11-02 09:41:20 +0900

    fix: script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 npm install
 npm run prisma:generate -w packages/server
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 HOST=127.0.0.1 PORT=3001 npm run start -w packages/server &
 HOST=127.0.0.1 PORT=3000 API_HOST=http://127.0.0.1:3001 npm run start -w packages/client &
